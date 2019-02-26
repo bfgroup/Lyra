06e347b340b30d5c9334e124b64f5f8cee5426e0
Rene Rivera <grafikrobot@gmail.com>
2019-02-25 18:35:17 -0600

    Translate mulitple flags tests.

---
+++ src/test_multiple_flags.cpp
@@ -0,0 +1,38 @@
+/*
+Copyright Rene Rivera 2019
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE_1_0.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#include <lyra/lyra.hpp>
+#include "mini_test.hpp"
+
+int main()
+{
+    using namespace lyra;
+    bfg::mini_test::scope test;
+
+    {
+        bool a = false, b = false, c = false;
+        auto cli = Opt( a )["-a"] | Opt( b )["-b"] | Opt( c )["-c"];
+        auto result = cli.parse({ "TestApp", "-a", "-b", "-c" });
+        test
+            (REQUIRE(result))
+            (REQUIRE(a))
+            (REQUIRE(b))
+            (REQUIRE(c));
+    }
+    {
+        bool a = false, b = false, c = false;
+        auto cli = Opt( a )["-a"] | Opt( b )["-b"] | Opt( c )["-c"];
+        auto result = cli.parse({ "TestApp", "-abc" });
+        test
+            (REQUIRE(result))
+            (REQUIRE(a))
+            (REQUIRE(b))
+            (REQUIRE(c));
+    }
+
+    return test;
+}
