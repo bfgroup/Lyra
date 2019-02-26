62f1a35ae8fd1d6ac55770f6166c08df1e522382
Rene Rivera <grafikrobot@gmail.com>
2019-02-25 23:06:30 -0600

    Fix refs to local license file.

--- include/lyra/lyra.hpp
+++ include/lyra/lyra.hpp
@@ -2,7 +2,7 @@
 // Copyright 2017 Two Blue Cubes Ltd. All rights reserved.
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef LYRA_HPP_INCLUDED
 #define LYRA_HPP_INCLUDED
