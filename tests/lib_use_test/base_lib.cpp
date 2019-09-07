e55e272fa28a7bc91b049dafb9271933a61c2d4b
Rene Rivera <grafikrobot@gmail.com>
2019-09-07 10:52:48 -0500

    Add external lib use test. [skip ci]

---
+++ tests/lib_use_test/base_lib.cpp
@@ -0,0 +1,25 @@
+/*
+Copyright Rene Rivera 2019
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#include <lyra/lyra.hpp>
+#include "base_lib.hpp"
+
+namespace L
+{
+int lib_args(int argc, const char ** argv)
+{
+	using namespace lyra;
+	bool flag = false;
+	auto p = opt( flag, "true|false" )
+			["-f"]
+			("A flag");
+
+	flag = false;
+	auto result = p.parse(args(argc, argv));
+	return bool(result);
+}
+}
