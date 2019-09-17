a472f029e838968e8ecc5ded825c92792320c534
Rene Rivera <grafikrobot@gmail.com>
2019-09-17 16:33:49 -0600

    Fix optional bound value test.

--- tests/optional_type_run_test.cpp
+++ tests/optional_type_run_test.cpp
@@ -10,28 +10,29 @@ http://www.boost.org/LICENSE_1_0.txt)
 
 int main()
 {
-    using namespace lyra;
-    bfg::mini_test::scope test;
+	using namespace lyra;
+	bfg::mini_test::scope test;
 
-    #if defined(LYRA_CONFIG_OPTIONAL_TYPE)
-    LYRA_CONFIG_OPTIONAL_TYPE<std::string> name;
-    auto p = opt(name, "name")
-        ["-n"]["--name"]
-        ("the name to use");
-    {
-        auto result = p.parse({ "TestApp", "-q", "Pixie" });
-        test
-            (REQUIRE( result ))
-            (REQUIRE( !name.has_value() ));
-    }
-    {
-        auto result = p.parse({ "TestApp", "-n", "Pixie" });
-        test
-            (REQUIRE( result ))
-            (REQUIRE( name.has_value() ))
-            (REQUIRE( name.value() == "Pixie" ));
-    }
-    #endif
+	#if defined(LYRA_CONFIG_OPTIONAL_TYPE)
+	LYRA_CONFIG_OPTIONAL_TYPE<std::string> name;
+	auto p = cli_parser()
+		| opt(name, "name")
+			["-n"]["--name"]
+			("the name to use");
+	{
+		auto result = p.parse({ "TestApp" });
+		test
+			(REQUIRE( result ))
+			(REQUIRE( !name.has_value() ));
+	}
+	{
+		auto result = p.parse({ "TestApp", "-n", "Pixie" });
+		test
+			(REQUIRE( result ))
+			(REQUIRE( name.has_value() ))
+			(REQUIRE( name.value() == "Pixie" ));
+	}
+	#endif
 
-    return test;
+	return test;
 }
