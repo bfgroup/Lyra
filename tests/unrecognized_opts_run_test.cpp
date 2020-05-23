66af199a746f313a290766b9145f6159a0c28581
Rene Rivera <grafikrobot@gmail.com>
2020-05-22 22:08:41 -0500

    Add a, disabled, test for issue #24.

--- tests/unrecognized_opts_run_test.cpp
+++ tests/unrecognized_opts_run_test.cpp
@@ -7,21 +7,36 @@ http://www.boost.org/LICENSE_1_0.txt)
 
 #include <lyra/lyra.hpp>
 #include "mini_test.hpp"
+#include <string>
 
 int main()
 {
-    using namespace lyra;
-    bfg::mini_test::scope test;
+	using namespace lyra;
+	bfg::mini_test::scope test;
 
-    {
-        bool a = false;
-        auto cli = cli_parser() | opt( a )["-a"];
-        auto result = cli.parse( { "TestApp", "-b" } );
-        test
-            (REQUIRE( !result ))
-            (REQUIRE( result.errorMessage().find( "Unrecognized token") != std::string::npos ))
-            (REQUIRE( result.errorMessage().find( "-b" ) != std::string::npos ));
-    }
+	{
+		bool a = false;
+		auto cli = cli_parser() | opt( a )["-a"];
+		auto result = cli.parse( { "TestApp", "-b" } );
+		test
+			(REQUIRE( !result ))
+			(REQUIRE( result.errorMessage().find( "Unrecognized token") != std::string::npos ))
+			(REQUIRE( result.errorMessage().find( "-b" ) != std::string::npos ));
+	}
+	#if 0 // Test case for https://github.com/bfgroup/Lyra/issues/24
+	{
+		bool o = false;
+		std::string a;
+		auto cli = cli_parser()
+			.add_argument(opt( o )["--option"].optional())
+			.add_argument(arg(a, "argument"));
+		auto result = cli.parse( { "TestApp", "--function", "arg" } );
+		test
+			(REQUIRE( !result ))
+			(REQUIRE( result.errorMessage().find( "Unrecognized token") != std::string::npos ))
+			(REQUIRE( result.errorMessage().find( "--function" ) != std::string::npos ));
+	}
+	#endif
 
-    return test;
+	return test;
 }
