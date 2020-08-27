85332b0fb378ebdfb87cdf2f8b73e4942552be49
Rene Rivera <grafikrobot@gmail.com>
2020-08-26 23:11:12 -0500

    Add missing docs for lyra::val.

--- include/lyra/val.hpp
+++ include/lyra/val.hpp
@@ -16,6 +16,17 @@ namespace lyra {
 [#lyra_val]
 = `lyra::val`
 
+[source]
+----
+auto val(T && v);
+auto val(const char * v);
+----
+
+Makes a bound self-contained value of the type of the given r-value. The created
+bound values can be used in place of the value references for arguments. And can
+be retrieved with the
+<<lyra_cli_array_ref>> call.
+
 */ // end::reference[]
 template <typename T>
 detail::BoundVal<T> val(T && v)
