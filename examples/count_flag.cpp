1f702bf312015ba3536f8286a364b8ca4c3bb5f6
Rene Rivera <grafikrobot@gmail.com>
2020-10-21 09:13:29 -0500

    Add example/doc for counting flags.

---
+++ examples/count_flag.cpp
@@ -0,0 +1,49 @@
+/*
+Copyright 2020 René Ferdinand Rivera Morell
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+
+/* tag::doc[]
+
+= Counting Flags
+
+Some times we want to allow multiple flags to be specified. This is most common
+with a verbose (`-v`) option. For example in passing in `-vvv` to indicate more
+verbose output from a program than using `-v`. To do this we can specify the
+cardinality as usual, and make use of passing in a lambda for the value of the
+option instead of a variable reference. With the lambda we can count the number
+of the flag occurrences.
+
+[source]
+----
+end::doc[] */
+
+// tag::doc[]
+#include <iostream>
+#include <lyra/lyra.hpp>
+
+int main(int argc, const char ** argv)
+{
+	int verbose = 0; // <1>
+	return lyra::main()
+		(lyra::opt(
+			[&](bool v){ verbose += 1; }) // <2>
+			["-v"].cardinality(0,5)) // <3>
+		(argc, argv, [&](lyra::main & m)
+		{
+			std::cout << verbose << "\n"; // <4>
+			return 0;
+		});
+}
+// end::doc[]
+/* tag::doc[]
+----
+<1> We start off the verbose as "off" by having zero count.
+<2> The option takes a lambda for the value that counts the flag each time
+the user specifies it.
+<3> We allow a verbose level up to 5, i.e. five `-v` flags can be specified.
+<4> We end up with the counted `verbose` level after parsing.
+end::doc[] */
---
+++ tests/ex_count_flag_run_test.cpp
@@ -0,0 +1,25 @@
+/*
+Copyright 2020 René Ferdinand Rivera Morell
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#include "main_test.hpp"
+
+#include "count_flag.cpp"
+
+#include "main_test.hpp"
+
+int main()
+{
+	bfg::mini_test::scope test;
+	{
+		TEST_MAIN(test, "count_flag");
+		TEST_MAIN(test, "count_flag", "-v");
+		TEST_MAIN(test, "count_flag", "-v", "-v", "-v");
+		TEST_MAIN(test, "count_flag", "-vvvvv");
+		TEST_MAIN_FAIL(test, "count_flag", "-vvvvvvvvvv");
+	}
+	return test;
+}
