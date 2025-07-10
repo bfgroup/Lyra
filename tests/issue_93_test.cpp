4d2e748c1eb8a534e00d9f753525edcab573db6d
Rene Rivera <grafikrobot@gmail.com>
2025-07-10 00:15:42 -0500

    Fix initializer list wankery from msvc compiler.

--- tests/issue_93_test.cpp
+++ tests/issue_93_test.cpp
@@ -16,9 +16,9 @@ int main()
 	auto cli = lyra::cli {};
 
 	auto f = [p = std::make_unique<int>(1)](bool) {};
-	cli.add_argument(lyra::opt { f });
+	cli.add_argument(lyra::opt(f));
 
 	auto g = [p = std::make_unique<int>(1)](std::string) {};
-	cli.add_argument(lyra::opt { g, "hint" });
-	cli.add_argument(lyra::arg { g, "hint" });
+	cli.add_argument(lyra::opt(g, "hint"));
+	cli.add_argument(lyra::arg(g, "hint"));
 }
