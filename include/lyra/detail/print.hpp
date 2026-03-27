0df74284908bd0eb911e1e50be0ffd01147036e8
Hermann von Kleist <hermann.von.kleist@ivi.fraunhofer.de>
2026-03-27 10:15:04 +0100

    Fix -Wundef warning in print.hpp

--- include/lyra/detail/print.hpp
+++ include/lyra/detail/print.hpp
@@ -6,15 +6,15 @@
 #ifndef LYRA_DETAIL_PRINT_HPP
 #define LYRA_DETAIL_PRINT_HPP
 
+#ifndef LYRA_DEBUG
+#	define LYRA_DEBUG 0
+#endif
+
 #if LYRA_DEBUG
 #	include <iostream>
 #endif
 
 #include <string>
-
-#ifndef LYRA_DEBUG
-#	define LYRA_DEBUG 0
-#endif
 
 namespace lyra { namespace detail {
 
